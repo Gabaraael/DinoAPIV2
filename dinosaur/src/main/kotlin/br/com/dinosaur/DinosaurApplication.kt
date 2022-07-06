835abfe319e81d0f2d943beae620f95c85763e6d
Gabaraael <gabriel1998.gp45@gmail.com>
2022-07-06 10:34:42 -0300

    Dinosaur project

---
+++ dinosaur/src/main/kotlin/br/com/dinosaur/DinosaurApplication.kt
@@ -0,0 +1,11 @@
+package br.com.dinosaur
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DinosaurApplication
+
+fun main(args: Array<String>) {
+	runApplication<DinosaurApplication>(*args)
+}
